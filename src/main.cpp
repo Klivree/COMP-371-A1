32454c344c1cbda8fe760bb4ff8c442d31712383
Jack Drolet <jjtelord@gmail.com>
2021-07-17 11:45:05 -0400

    Added csv shape renderer

--- src/main.cpp
+++ src/main.cpp
@@ -94,52 +94,149 @@ GLuint compileAndLinkShaders() {
     return shaderProgram;
 }
 
-GLuint getObject() {
-    GLfloat vertices[] = {
-        0.0f * scale, 0.0f * scale, 0.0f * scale, //0: lower left of L long
-        0.0f * scale, 5.0f * scale, 0.0f * scale, //1: upper left of L long
-        1.0f * scale, 0.0f * scale, 0.0f * scale, //2: lower right of L long
-        1.0f * scale, 5.0f * scale, 0.0f * scale, //3: upper right of L long
-        1.0f * scale, 1.0f * scale, 0.0f * scale, //4: upper left of L leg
-        3.0f * scale, 1.0f * scale, 0.0f * scale, //5: upper right of L leg
-        3.0f * scale, 0.0f * scale, 0.0f * scale, //6: lower right of L leg
+GLuint getCubeModel(glm::vec3 color) {
+    // Cube model
+    glm::vec3 vertexArray[] = {  
+        // position,                  color
+        glm::vec3(-0.5f,-0.5f,-0.5f), color, // left
+        glm::vec3(-0.5f,-0.5f, 0.5f), color,
+        glm::vec3(-0.5f, 0.5f, 0.5f), color,
+
+        glm::vec3(-0.5f,-0.5f,-0.5f), color,
+        glm::vec3(-0.5f, 0.5f, 0.5f), color,
+        glm::vec3(-0.5f, 0.5f,-0.5f), color,
+
+        glm::vec3(0.5f, 0.5f,-0.5f), color, // far
+        glm::vec3(-0.5f,-0.5f,-0.5f), color,
+        glm::vec3(-0.5f, 0.5f,-0.5f), color,
+
+        glm::vec3(0.5f, 0.5f,-0.5f), color,
+        glm::vec3(0.5f,-0.5f,-0.5f), color,
+        glm::vec3(-0.5f,-0.5f,-0.5f), color,
+
+        glm::vec3(0.5f,-0.5f, 0.5f), color, // bottom
+        glm::vec3(-0.5f,-0.5f,-0.5f), color,
+        glm::vec3(0.5f,-0.5f,-0.5f), color,
+
+        glm::vec3(0.5f,-0.5f, 0.5f), color,
+        glm::vec3(-0.5f,-0.5f, 0.5f), color,
+        glm::vec3(-0.5f,-0.5f,-0.5f), color,
+
+        glm::vec3(-0.5f, 0.5f, 0.5f), color, // near
+        glm::vec3(-0.5f,-0.5f, 0.5f), color,
+        glm::vec3(0.5f,-0.5f, 0.5f), color,
+
+        glm::vec3(0.5f, 0.5f, 0.5f), color,
+        glm::vec3(-0.5f, 0.5f, 0.5f), color,
+        glm::vec3(0.5f,-0.5f, 0.5f), color,
+
+        glm::vec3(0.5f, 0.5f, 0.5f), color, // right
+        glm::vec3(0.5f,-0.5f,-0.5f), color,
+        glm::vec3(0.5f, 0.5f,-0.5f), color,
+
+        glm::vec3(0.5f,-0.5f,-0.5f), color,
+        glm::vec3(0.5f, 0.5f, 0.5f), color,
+        glm::vec3(0.5f,-0.5f, 0.5f), color,
+
+        glm::vec3(0.5f, 0.5f, 0.5f), color, // top
+        glm::vec3(0.5f, 0.5f,-0.5f), color,
+        glm::vec3(-0.5f, 0.5f,-0.5f), color,
+
+        glm::vec3(0.5f, 0.5f, 0.5f), color,
+        glm::vec3(-0.5f, 0.5f,-0.5f), color,
+        glm::vec3(-0.5f, 0.5f, 0.5f), color,
     };
 
-    GLuint indices[] = {
-        0,1,2,
-        1,3,2,
-        4,5,2,
-        2,6,5
-    };
-
-
-    //create VAO
-    GLuint VAO, VBO, EBO;
+    GLuint VAO, VBO;
+    //create the Vertex Array Object
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
     glBindVertexArray(VAO);
 
+    //create Vertex Buffer Object
+    glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexArray), vertexArray, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    //get position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+    //create position attribute
+    glVertexAttribPointer(0, 3,  GL_FLOAT,  GL_FALSE, 2 * sizeof(glm::vec3), (void*)0);
     glEnableVertexAttribArray(0);
 
-    //unbind the VAO and VBO
+    //create color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(glm::vec3), (void*)sizeof(glm::vec3));
+    glEnableVertexAttribArray(1);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); //unbind EBO after VAO so VAO knows it should keep using the EBO
 
     return VAO;
 }
 
+void renderShapeFromCSV(string filePath, glm::vec3 pos, GLfloat scale, GLuint shaderProgram) {
+    /** Renders the shape specified in the CSV file provided by using transformed cube models. A cube model should be binded before this method is called.
+    * filePath - filePath of the .csv file that defines the shape
+    * pos - coordinates of the object
+    * scale - how much the object is to be scaled by 
+    * shaderProgram - shader program used by the running OpenGL application
+    * 
+    * Each line in the csv file renders a seperate rectangular prism. The line should be formatted as such:
+    * local x coordinate, local y coordinate, local z coordinate, local x scale, local y scale, local z scale
+    **/
 
+    ifstream fileStream(filePath, ios::in);
+
+    if (!fileStream.is_open()) {
+        cerr << "Could not read file " << filePath << ". File does not exist." << endl;
+        return;
+    }
+
+    string value, line = "";
+    float cubeInfo[6] = {};
+    int i, j;
+    char curChar;
+    bool cubeProcessed;
+    GLuint worldMatrixLocation = glGetUniformLocation(shaderProgram, "worldMatrix");
+
+    while (!fileStream.eof()) {
+        getline(fileStream, line); //get line for a cube
+        line.append("\n");
+
+        value = "";
+        i = 0; // curChar index
+        j = 0; // cubeInfo index
+
+        // parse the line to get cube info in the form of: 
+        //(local x coord,       local y coord,      local z coord,      size in x dir,      size in y dir,      size in z dir)
+        while (true) {
+            curChar = line[i++];
+
+            if (curChar == '\n' || curChar == ',') {//need to push the info when these chars appear
+                cubeInfo[j++] = stof(value); // push float value to the array
+                value = "";
+            }
+            else
+                value.append(string(1, curChar)); // add the char to the current value
+
+            // line is finished
+            if (curChar == '\n') 
+                break;
+        }
+
+        if (j != 6) { //if we didnt get to 6 then we either have too many or too little values per line
+            cerr << "There should be exclusively 6 values per line of the csv" << endl;
+            return;
+        }
+       
+        //scale the values
+        for (int k = 0; k < 6; k++) {
+            cubeInfo[k] = scale * cubeInfo[k];
+        }
+
+        //now we draw the cube
+        glm::mat4 cubeWorldMatrix = glm::translate(glm::mat4(1.0), glm::vec3(pos.x + cubeInfo[0], pos.y + cubeInfo[1], pos.z + cubeInfo[2])) * glm::scale(glm::mat4(1.0), glm::vec3(cubeInfo[3], cubeInfo[4], cubeInfo[5]));
+        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &cubeWorldMatrix[0][0]);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+}
 
 int main(int argc, char* argv[]) {
 	glfwInit(); //initialize GLFW
@@ -179,17 +276,25 @@ int main(int argc, char* argv[]) {
     //get shader program
     GLuint shaderProgram = compileAndLinkShaders();
 
-    //get VAO
-    GLuint VAO = getObject();
+    //get Shape cubes
+    GLuint shapeVAO = getCubeModel(glm::vec3(1.0f, 1.0f, 0.0f));
+
+    GLuint wallVAO = getCubeModel(glm::vec3(0.5f, 0.5f, 0.5f));
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT); //clear the window each frame
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
 
-        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+        //draw the shapes
+        glBindVertexArray(shapeVAO);
+        renderShapeFromCSV("shapefilepath.csv", glm::vec3(0.0,0.0,0.0), 0.2, shaderProgram);
+        glBindVertexArray(0);
 
 
+        //draw the wall models
+        glBindVertexArray(wallVAO);
+        renderShapeFromCSV("shapeHoleFilepath.csv", glm::vec3(0.0,0.0,2.0), 0.2, shaderProgram);
+        glBindVertexArray(0);
 
 
         glfwSwapBuffers(window); //swap the front buffer with back buffer
