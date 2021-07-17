8f86376872dc94a829d1cbd7e461379fca4c8950
J-Drolet <79327056+J-Drolet@users.noreply.github.com>
2021-07-17 17:38:37 -0400

    Update vertexshader.glsl
    Updated shader with matrices

--- resources/vertexshader.glsl
+++ resources/vertexshader.glsl
@@ -1,8 +1,14 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
 out vec3 vertexColor;
+
+uniform mat4 worldMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix = mat4(1.0);
+
 void main()
 {
-	vertexColor = vec3(1.0f, 1.0f, 1.0f);
-	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	vertexColor = aColor;
+	gl_Position = projectionMatrix * viewMatrix * worldMatrix * vec4(aPos, 1.0);
 }
