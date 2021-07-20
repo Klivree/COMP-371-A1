ba18efb503524947dade65001f38ff922c2bfd3f
J-Drolet <79327056+J-Drolet@users.noreply.github.com>
2021-07-20 19:19:06 -0400

    Update A1.cpp
    Add the origin lines

--- src/A1.cpp
+++ src/A1.cpp
@@ -246,6 +246,8 @@ void renderGrid(GLuint shaderProgram);
 
 void executeEvents(GLFWwindow* window, Camera &camera, float dt);
 
+void renderLine(glm::vec3 pos, glm::vec3 size, glm::vec3 color, GLfloat scale, GLuint shaderProgram);
+
 int main(int argc, char* argv[]) {
 	glfwInit(); //initialize GLFW
 
@@ -328,6 +330,11 @@ int main(int argc, char* argv[]) {
 
 	glBindVertexArray(0);
 
+	//render the origin lines
+        renderLine(glm::vec3(0.0f), glm::vec3(5.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), scale, shaderProgram); // x direction
+        renderLine(glm::vec3(0.0f), glm::vec3(0.0f, 5.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), scale, shaderProgram); // y direction
+        renderLine(glm::vec3(0.0f), glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f, 0.0f, 1.0f), scale, shaderProgram); // z direction
+	    
         //end frame
         glfwSwapBuffers(window); //swap the front buffer with back buffer
         glfwPollEvents(); //get inputs
@@ -495,4 +502,52 @@ void renderGrid(GLuint shaderProgram) {
 			glDrawArrays(GL_LINES, 0, 8);
 		}
 	}
+}
+
+void renderLine(glm::vec3 pos, glm::vec3 size, glm::vec3 color, GLfloat scale, GLuint shaderProgram) {
+    /* Renders a line without the need to first load a VAO.
+    *   pos - position to start drawing the line
+    *   size - vector away from origin the other point is at
+    *   color - color of the line
+    *   scale - world scale to be used
+    *   shaderProgram - shader program of the current application
+    */
+
+    glm::vec3 vertexArray[] = {
+       pos, color,
+       pos + (size * scale), color
+    };
+
+    GLuint VAO, VBO;
+    //create the Vertex Array Object
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    //create Vertex Buffer Object
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexArray), vertexArray, GL_STATIC_DRAW);
+
+    //create position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(glm::vec3), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //create color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(glm::vec3), (void*)sizeof(glm::vec3));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    
+    GLuint worldMatrixLocation = glGetUniformLocation(shaderProgram, "worldMatrix");
+
+    //draw the line
+    glm::mat4 worldMatrix = glm::mat4(1.0); //ensure no transformation
+    glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]);
+    glDrawArrays(GL_LINES, 0, 2);
+
+    glBindVertexArray(0);
+
+    //delete vertex arrays and buffer to prevent memory leak
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 }
